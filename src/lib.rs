4e0381e63ab52d695f47e20e4c6e23f674baf668
ckounter <gladunov.denis@gmail.com>
2025-01-05 20:55:06 +0300

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,35 @@
 #[macro_export]
 macro_rules! dowhile {
     ($body:block $cond:expr) => ({
-        let mut r#dowhile_flag = true;
-        while r#dowhile_flag || ($cond) {
-            r#dowhile_flag = false;
+        loop {
             $body
+            if !$cond {
+                break;
+            }
         }
     });
     ($label:lifetime: $body:block $cond:expr) => ({
-        let mut r#dowhile_flag = true;
-        $label: while r#dowhile_flag || ($cond) {
-            r#dowhile_flag = false;
+        $label: loop {
             $body
+            if !$cond {
+                break;
+            }
         }
     });
     ($cond:expr, $body:block) => ({
-        let mut r#dowhile_flag = true;
-        while r#dowhile_flag || ($cond) {
-            r#dowhile_flag = false;
+        loop {
             $body
+            if !$cond {
+                break;
+            }
         }
     });
     ($cond:expr, $label:lifetime: $body:block) => ({
-        let mut r#dowhile_flag = true;
-        $label: while r#dowhile_flag || ($cond) {
-            r#dowhile_flag = false;
+        $label: loop {
             $body
+            if !$cond {
+                break;
+            }
         }
     });
 }
